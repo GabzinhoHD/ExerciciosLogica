d2988c0028151d08aaed49404d1293421cde564b
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-04-27 13:57:53 -0300

    Update Ex5.c
    Finalizando e + ou - otimizado

--- Exercicios(Lista-4)/Ex5.c
+++ Exercicios(Lista-4)/Ex5.c
@@ -5,7 +5,7 @@ int main()
     float valorCompra, valorPag, valorParcela, valorDif;
     int opcao, parcelas;
     
-    printf("\t-----MENU DE PAGAMENTO-----\n\n");
+    printf("-----MENU DE PAGAMENTO-----\n\n");
     printf("Informe o valor total da compra\n");
     scanf("%f", &valorCompra);
     
@@ -22,15 +22,56 @@ int main()
     switch(opcao){
         case 1:
             valorPag = valorCompra * 0.85;
+            parcelas = 1;
             valorDif = valorCompra - valorPag;
-            if(valorCompra > valorPag){
-                printf("| Valor da Compra | Valor do Pagamento   | Diferença dos Valores |\n");
-                printf("| R$%.2f          | R$%.2f               | R$%.2f                |", valorCompra, valorPag, valorDif);
-            }
+            valorParcela = valorPag / parcelas;
             break;
+            
+        case 2:
+            valorPag = valorCompra * 0.9;
+            parcelas = 1; 
+            valorDif = valorCompra - valorPag;
+            valorParcela = valorPag / parcelas;
+            break;
+            
+        case 3: 
+            valorPag = valorCompra * 0.95;    
+            parcelas = 3;
+            valorDif = valorCompra - valorPag;
+            valorParcela = valorPag / parcelas;
+            break;
+            
+        case 4: 
+            valorPag = valorCompra; 
+            parcelas  = 6;
+            valorDif = valorCompra - valorPag;
+            valorParcela = valorPag / parcelas;
+            break; 
+            
+        case 5: 
+            valorPag = valorCompra * 1.08;
+            parcelas = 12;
+            valorDif = valorPag - valorCompra;
+            valorParcela = valorPag / parcelas;
+            break;
+            
         default:
-            printf("Valor Incorreto!!");
+            printf("Entrada Invalida.\n");
+            return 0;
+    }    
+        
+    if(opcao == 5){
+        printf("---RESULTADO DA COMPRA---\n");
+        printf("| Valor Total    : R$%.2f\n", valorCompra);
+        printf("| Quan. Parcelas : %d    \n", parcelas);
+        printf("| Valor Acréscimo: R$%.2f\n", valorDif);
+        printf("| Valor Final    : R$%.2f\n", valorParcela);
     }
-
-    return 0;
+    else{
+        printf("---RESULTADO DA COMPRA---\n");
+        printf("| Valor Total   : R$%.2f\n", valorCompra);
+        printf("| Quan. Parcelas: %d    \n", parcelas);
+        printf("| Valor Desconto: R$%.2f\n", valorDif);
+        printf("| Valor Final   : R$%.2f\n", valorParcela);
+    }
 }
