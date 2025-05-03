1db0289539c692c5cbaa5a27fed3a3cb9b522a3d
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-02 21:12:30 -0300

    Create Ex2.c

---
+++ Exercicios(Lista-5)/Ex2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 15;
+    printf("---CONTADOR DE 15 Á 0 ---\n");
+    
+    while(i >= 0){
+        printf("%d\n", i);
+        i--;
+    }
+    return 0;
+}
