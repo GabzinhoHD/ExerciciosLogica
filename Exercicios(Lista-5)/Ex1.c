26819eb4a08e92b22d8a4f266f1d79f72e9ee079
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-02 21:10:41 -0300

    Create Ex1.c

---
+++ Exercicios(Lista-5)/Ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    printf("---CONTADOR DE 1 Á 20---\n");
+    
+    while(i <= 20){
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}
