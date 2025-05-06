e6a9f8c886b4e35c765372184495caeb900cc5e2
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-06 11:40:57 -0300

    Create Ex6.c

---
+++ Exercicios(Lista-5)/Ex6.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int  num, i = 1, soma = 0, quantNum = 0;
+    float media;
+    
+    printf("---MEDIA ARITMETICA---\n");
+    printf("Insira 15 valores(Maiores que 10!\n");
+    while(i <= 15){
+        scanf("%d", &num);
+            if(num > 10){
+                soma = soma + num;
+                quantNum++;
+            }
+        i++;
+    }
+    
+    if(quantNum > 0){
+        media = soma / quantNum;
+        printf("A media é: %f", media);
+    }
+    else{
+        printf("Os valores são menores que 10!!");
+    }
+
+    return 0;
+}
