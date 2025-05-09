2477f3c6a0fb65fb7c9ac3ba18ba37f15930f3ab
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-09 10:46:13 -0300

    Create Ex7.c

---
+++ Exercicios(Lista-6)/Ex7.c
@@ -0,0 +1,47 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main()
+{
+    int i = 1, numA = 0, numC = 0, numF = 0;
+    float perA, perC, perF;
+    char torce;
+    
+    printf("---TORCIDAS DO PARANÁ---\n");
+    do{
+        printf("--INFORME SEU TIME--\n");
+        printf("[A] - Atletico\n[C] - Coritiba\n[F] - Foz\n[S] - Sair\n");
+        scanf(" %c", &torce);
+        torce = toupper(torce);
+        if(torce != 'S'){
+            if(torce == 'A'){
+                numA++;
+                i++;
+            }
+            else if(torce == 'C'){
+                numC++;
+                i++;
+            }
+            else if(torce == 'F'){
+                numF++;
+                i++;
+            }
+            else{
+                printf("Entrada Invalida!!\n");
+            }
+        }
+        
+    }while(torce != 'S');
+    i--;
+    
+    perA = (float)numA / i;
+    perC = (float)numC / i;
+    perF = (float)numF / i;
+    
+    printf("Total de Entrevistados: %d\n", i);
+    printf("Torcedores do Atletico: %d, Percentual: %.1f\n", numA, perA);
+    printf("Torcedores do Coritiba: %d, Percentual: %.1f\n", numC, perC);
+    printf("Torcedores do Foz     : %d, Percentual: %.1f\n", numF, perF);
+
+    return 0;
+}
