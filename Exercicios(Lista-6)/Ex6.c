a1df09c0d023247b2d00256b5cd5bde0300ada0f
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-09 10:25:07 -0300

    Create Ex6.c

---
+++ Exercicios(Lista-6)/Ex6.c
@@ -0,0 +1,39 @@
+#include <ctype.h>
+#include <stdio.h>
+
+int main()
+{
+    int i = 1;
+    float notaA, notaB, media;
+    char continua;
+    
+    printf("---MEDIA DAS NOTAS A & B---\n");
+    do{
+       printf("Aluno %d\n", i);
+       printf("Entre com a nota A: ");
+       scanf("%f", &notaA);
+       printf("Entre com a nota B: ");
+       scanf("%f", &notaB);
+       
+       media = (notaA + (notaB * 2)) / 2;
+       printf("Media do aluno %d = %.1f\n", i, media);
+       
+       do{
+           printf("Continuar?(S/N): ");
+           scanf(" %c", &continua);
+           continua = toupper(continua);
+           if( continua == 'N'){
+               break;
+           }
+           
+           else if(continua != 'S' && continua != 'N'){
+               printf("ERRO: Resposta Invalida!!\n");
+           }
+           
+       }while(continua != 'S');
+       
+       i++;
+    }while(continua == 'S');
+
+    return 0;
+}
