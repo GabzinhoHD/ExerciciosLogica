08a15f657280f9be4e4c84dffce781a27c7ed7f4
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-23 11:24:51 -0300

    Update Ex2.c

--- (Lista-7)For/Ex2.c
+++ (Lista-7)For/Ex2.c
@@ -2,11 +2,35 @@
 
 int main()
 {
-    int i;
-    float valor, valorMax, valorMin, media;
-    
-    for(; i <= 30, i++){
+    int i, qtd = 0;
+    float soma = 0, valor, valorMax, valorMin, media;
+
+    for(i = 1; i <= 30; i++){
+        printf("Digite o %iº numero: ", i);
+        scanf("%f", &valor);
         
+        /* soma para a media */
+        if(valor > 5.9){
+            soma = soma + valor;
+            qtd++;
+        }
+        
+        /* determinação do max e min */
+        if(i == 1){
+            valorMax = valor;
+            valorMin = valor;
+        }
+        else {
+            if (valor > valorMax) valorMax = valor;
+            if (valor < valorMin) valorMin = valor;
+        }
     }
+    
+    media = soma / qtd;
+    
+    printf("O maior valor é: %.2f\n", valorMax);
+    printf("O menor valor é: %.2f\n", valorMin);
+    printf("A media dos valores maiores que 5,9 é: %.2f", media);
+    
     return 0;
 }
