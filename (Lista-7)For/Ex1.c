ba30de5fe22b0bbe990aa7a571de36a5f1efa245
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-23 12:02:05 -0300

    Update Ex1.c

--- (Lista-7)For/Ex1.c
+++ (Lista-7)For/Ex1.c
@@ -3,17 +3,15 @@
 int main()
 {
     int i, n, m;
+    
     printf("---TABUADA---\n");
     printf("Informe o valor multiplicador: ");
     scanf("%d", &n);
     printf("Informe a quantidade de multiplicações: ");
     scanf("%d", &m);
     
-    
     for(i = 1;i <= m; i++){
         printf("%d X %d = %d\n", n, i, i*n);
     }
-    
-
     return 0;
 }
