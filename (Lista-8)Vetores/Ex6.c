fb9a42ed43f33c61b1974dfadc434fc7ee7827f8
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-28 12:36:26 -0300

    Create Ex6.c

---
+++ (Lista-8)Vetores/Ex6.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, tam = 20, menorValor = 0, posicao = 0, vetorV[tam];
+    
+    for(i = 0; i < tam; i++){
+        do{
+            printf("Informe o %dº valor: ", i+1);
+            scanf("%d", &vetorV[i]);
+            
+            if(i == 0){
+                menorValor = vetorV[i];
+                posicao = i;
+            }
+            
+            else if(vetorV[i] < menorValor){
+                menorValor = vetorV[i];
+                posicao = i;
+            }
+        
+        }while(vetorV[i] < 0);
+    }
+    
+    printf("O menor valor é: %d\n", menorValor);
+    printf("Na posição %d do vetor", posicao+1);
+    
+    
+    return 0;
+}
