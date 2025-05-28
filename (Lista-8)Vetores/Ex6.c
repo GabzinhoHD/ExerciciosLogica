04255aafec1d30ac6d975877f846d1ba0bf0f859
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-28 13:18:14 -0300

    Update Ex6.c

--- (Lista-8)Vetores/Ex6.c
+++ (Lista-8)Vetores/Ex6.c
@@ -3,7 +3,8 @@
 int main()
 {
     int i, tam = 20, menorValor = 0, posicao = 0, vetorV[tam];
-    
+
+    printf("---DETERMINADOR DO MENOR VALOR---\n");
     for(i = 0; i < tam; i++){
         do{
             printf("Informe o %dº valor: ", i+1);
@@ -24,7 +25,7 @@ int main()
 
     printf("\n---RESULTADO---\n");
     printf("O menor valor é: %d\n", menorValor);
-    printf("Na posição %d do vetor", posicao+1);
+    printf("Na %dº posição do vetor", posicao+1);
     
     
     return 0;
