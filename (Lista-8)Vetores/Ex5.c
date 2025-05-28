76768dfa187eaa2eac2511e96e42dd30b72087c4
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-28 12:19:11 -0300

    Create Ex5.c

---
+++ (Lista-8)Vetores/Ex5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, tam = 20, maiorValor = 0, posicao = 0, vetorV[tam];
+    
+    for(i = 0; i < tam; i++){
+        do{
+            printf("Informe o %dº valor: ", i+1);
+            scanf("%d", &vetorV[i]);
+            
+            if(vetorV[i] > maiorValor){
+                maiorValor = vetorV[i];
+                posicao = i;
+            }
+        
+        }while(vetorV[i] < 0);
+    }
+    
+    printf("O maior valor é: %d\n", maiorValor);
+    printf("Na posição %d do vetor", posicao+1);
+    
+    
+    return 0;
+}
