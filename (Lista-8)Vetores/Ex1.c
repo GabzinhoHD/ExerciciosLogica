aaff7200f6785c2c63bc8bef97ae182f1fe370c0
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-05-28 11:07:27 -0300

    Update Ex1.c

--- (Lista-8)Vetores/Ex1.c
+++ (Lista-8)Vetores/Ex1.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
 
+int main()
+{
+    int i, vetorA[5], vetorB[5];
+    
+    printf("---CALCULADOR DO DOBRO DE 5 VALORES---\n");
+    for(i = 0; i < 5; i++){
+        printf("Informe o %dº valor: ", i+1);
+        scanf("%d", &vetorA[i]);
+        vetorB[i] = vetorA[i] * 2;
+    }
+    
+    printf("\n---RESULTADO---\n");
+    
+    for(i = 0; i < 5; i++){
+        printf("O dobro do %dº valor é: %d\n", i+1, vetorB[i]);
+    }
+    return 0;
+}
