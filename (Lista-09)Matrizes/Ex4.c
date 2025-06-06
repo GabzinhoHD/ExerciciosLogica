61f672f38f8a3181cc327ebda31db23aa13c69d9
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-06-06 15:27:22 -0300

    Create Ex4.c

---
+++ (Lista-09)Matrizes/Ex4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, m, n, tam = 100;
+    int matrizA[tam][tam], matrizT[tam][tam];
+    
+    printf("---TRANSPOSIÇÃO DE MATRIZ---\n");
+    
+    do{
+        printf("Informe a quantidade de linhas: ");
+        scanf("%d", &m);
+        printf("Informe a quantidade de colunas: ");
+        scanf("%d", &n);
+        if(m <= 0 && n <= 0) printf("Impossivel forma uma matriz\n\n");
+        
+    }while(m <= 0 && n <= 0);
+    
+    printf("\nMATRIZ:\n");
+    for(i = 0; i < m; i++) for(j = 0; j < n; j++){
+        printf("\nlinha[%d], coluna[%d]\n", i+1, j+1);
+        printf("Informe valor: ");
+        scanf("%d", &matrizA[i][j]);
+    }
+    
+    /* transposição da matriz */
+      for(int i = 0; i < m; i++) for(int j = 0; j < n; j++){
+        matrizT[j][i] = matrizA[i][j];
+    }
+    
+   
+    printf("\n---RESULTADO---\n");
+    printf("\nMATRIZ NORMAL:\n");
+    for(int i = 0; i < m; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", matrizA[i][j]);
+        }
+        printf("\n");
+    }
+    
+    printf("\nMATRIZ TRANSPOSTA:\n");
+     for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            printf("%d ", matrizT[i][j]);
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
