387a43c0c63b5d783dc330d702fd3c2eecc3abf7
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-06-06 14:11:49 -0300

    Update Ex3.c

--- (Lista-09)Matrizes/Ex3.c
+++ (Lista-09)Matrizes/Ex3.c
@@ -2,35 +2,42 @@
 
 int main()
 {
-    int i, j, n, tam = 100;
+    int i, j, m, n, l, c, tam = 100;
     int matrizA[tam][tam], matrizB[tam][tam], matrizC[tam][tam];
     
     printf("---SOMADOR DE DUAS MATRIZES---\n");
     
-    printf("Informe o tamanho das matrizes: ");
-    scanf("%d", &n);
+    do{
+        printf("Informe a quantidade de linhas: ");
+        scanf("%d", &m);
+        printf("Informe a quantidade de colunas: ");
+        scanf("%d", &n);
+        if(m <= 0 && n <= 0) printf("Impossivel forma uma matriz\n\n");
+        
+    }while(m <= 0 && n <= 0);
     
-    for(i = 0; i < n; i++) for(j = 0; j < n; j++){
-        printf("\n---MATRIZ A---\n");
-        printf("linha[%d], coluna[%d]\n", i, j);
+    printf("\n---MATRIZ A---\n");
+    for(i = 0; i < m; i++) for(j = 0; j < n; j++){
+        printf("\nlinha[%d], coluna[%d]\n", i, j);
         printf("Informe valor: ");
         scanf("%d", &matrizA[i][j]);
     }
     
-    for(i = 0; i < n; i++) for(j = 0; j < n; j++){
-        printf("\n---MATRIZ B---\n");
-        printf("linha[%d], coluna[%d]\n", i, j);
+    printf("\n---MATRIZ B---\n");
+    for(i = 0; i < m; i++) for(j = 0; j < n; j++){
+        printf("\nlinha[%d], coluna[%d]\n", i, j);
         printf("Informe valor: ");
         scanf("%d", &matrizB[i][j]);
     }
     
-    for(i = 0; i < n; i++) for(j = 0; j < n; j++){
-       matrizC[i][j] = matrizA[i][j] + matrizB[i][j];
+    for(i = 0, l = 0; i < m; i++, l++) for(j = 0, c = 0; j < n; j++, c++){
+       matrizC[l][c] = matrizA[i][j] + matrizB[i][j];
     }
     
-    printf("\n---RESULTADO---\n\n");
-    for(i = 0; i < n; i++) for(j = 0; j < n; j++){
-        printf("lin[%d], col[%d] = [%d]\n", i, j, matrizC[i][j]);
+    printf("\n---RESULTADO---\n");
+    printf("---MATRIZ C---\n");
+    for(l = 0; l < m; l++) for(c = 0; c < n; c++){
+        printf("lin[%d], col[%d] = [%d]\n", l, c, matrizC[l][c]);
     }
     
     return 0;
