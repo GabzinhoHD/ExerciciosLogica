c2606c0c47a71031ee64fb273aaa94145be51cd5
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-06-12 13:45:15 -0300

    Update Ex1.c

--- (Lista-10)Strings/Ex1.c
+++ (Lista-10)Strings/Ex1.c
@@ -3,16 +3,15 @@
 
 int main()
 {
-    int i;
     char string1[100] = "lua", string2[100] = "cheia";
     
     printf("---CONCATENAÇÃO DE STRINGS(2º na 1º)---\n");
     
-    printf("String1: %s\nString2: %s\n", string1, string2);
+    printf("String1: %s  String2: %s\n", string1, string2);
     strcat(string1, string2);
     
     printf("\n---RESULTADO---\n");
-    printf("String1: %s\nString2: %s\n", string1, string2);
+    printf("String1: %s  String2: %s\n", string1, string2);
     
     return 0;
 }
