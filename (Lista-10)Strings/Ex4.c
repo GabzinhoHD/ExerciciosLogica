560a74c6e581d44451f27b105f1b9e37adee151f
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-06-12 14:26:03 -0300

    Create Ex4.c

---
+++ (Lista-10)Strings/Ex4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main()
+{
+    int i, qtd;
+    char string[100], caractere;
+    
+    printf("---CONTADOR DE CARACTERE---\n");
+    
+    printf("\nENTRADA:\n");
+    printf("Digite sua String: ");
+    fgets(string, sizeof(string), stdin);
+    
+    printf("Informe o caractere a ser contado: ");
+    scanf("%c", &caractere);
+    
+    for(i = 0; i < strlen(string); i++){
+        if(string[i] == caractere || string[i] == toupper(caractere)){
+            qtd++;
+        }
+    }
+    
+    printf("\n---RESULTADO---\n");
+    printf("String: %s", string);
+    printf("Nº de %c's: %d", caractere, qtd);
+    
+    return 0;
+}
