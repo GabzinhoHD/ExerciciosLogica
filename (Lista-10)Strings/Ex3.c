6f25576d6d281f6e6dfc8da622f01647505354e4
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-06-12 14:09:29 -0300

    Create Ex3.c

---
+++ (Lista-10)Strings/Ex3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i, qtd = 0;
+    char string[100];
+    
+    printf("---CONTADOR DE O's---\n");
+    
+    printf("Digite sua String: ");
+    fgets(string, sizeof(string), stdin);
+    
+    for(i = 0; i < strlen(string); i++){
+        if(string[i] == 'o' || string[i] == 'O'){
+            qtd++;
+        }
+    }
+    
+    printf("\n---RESULTADO---\n");
+    printf("String: %s", string);
+    printf("Nº de O's: %d", qtd);
+    
+    return 0;
+}
