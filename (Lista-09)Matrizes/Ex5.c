8fe88a922840932f36254d2a71c5022f118dd784
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-06-13 15:48:05 -0300

    Update Ex5.c

--- (Lista-09)Matrizes/Ex5.c
+++ (Lista-09)Matrizes/Ex5.c
@@ -15,10 +15,9 @@ int main()
         scanf("%d", &matriz[i][j]);
     }
     
-    
     printf("\n---MENU---\n");
     printf("| [a] | Mostrar matriz.\n");
-    printf("| [b] | Mostrar linha[4] coluna[1].\n");
+    printf("| [b] | Mostrar quarta linha e primeira coluna.\n");
     printf("| [c] | Mostrar soma da diagonal principal.\n");
     printf("| [d] | Atribuir {0} a todo negativo fora da diagonal.\n");
     printf("| [e] | Buscar um elemento na matriz.\n");
@@ -30,8 +29,8 @@ int main()
         if(opcao != 'A' && opcao != 'B' && opcao != 'C' && opcao != 'D' && opcao != 'E') printf("Entrada Invalida!!");
     }while(opcao != 'A' && opcao != 'B' && opcao != 'C' && opcao != 'D' && opcao != 'E');
     
-
     
+    /* Resultado das opções */
     switch(opcao){
         case 'A':
             printf("\n---RESULTADO---\n");
@@ -45,11 +44,11 @@ int main()
         
         case 'B':
             printf("\n---RESULTADO---\n");
-            printf("Linha[4]:\n");
-            for(i = 0; i < tam; i++) printf("[%d] ", matriz[i][0]);
+            printf("Quarta Linha:\n");
+            for(j = 0; j < tam; j++) printf("[%d] ", matriz[4][j]);
             printf("\n\n");
-            printf("Coluna[1]:\n");
-            for(j = 0; j < tam; j++) printf("[%d]\n", matriz[4][j]);
+            printf("Primeira Coluna:\n");
+            for(i = 0; i < tam; i++) printf("[%d]\n", matriz[i][0]);
         break;
         
         case 'C':
@@ -74,18 +73,18 @@ int main()
         break;
         
         case 'E':
-            
-            printf("Informe o elemento a ser localizado: ");
+            printf("\nInforme o elemento a ser localizado: ");
             scanf("%d", &busca);
             for(i = 0; i < tam; i++) for( j = 0; j < tam; j++){
                 if(busca == matriz[i][j]){
                     printf("\n---RESULTADO---\n");
-                    printf("Elemento localizado na linha[%d], coluna[%d].", i+1, j+1);
+                    printf("Elemento localizado na linha[%d], coluna[%d].", i, j);
                     return 0;
                 }
             }
             printf("Elemento não localizado!!\n");
         break;
     }
+    
     return 0;
 }
