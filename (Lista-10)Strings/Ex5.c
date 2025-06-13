fd7016f86bfacb018f2dddf7f0c55b35c1daa62c
GabzinhoHD <134400952+GabzinhoHD@users.noreply.github.com>
2025-06-13 15:55:26 -0300

    Update Ex5.c

--- (Lista-10)Strings/Ex5.c
+++ (Lista-10)Strings/Ex5.c
@@ -10,7 +10,7 @@ int main()
     printf("---MAIUSCULIZAÇÃO---\n");
     
     printf("\nENTRADA:\n");
-    printf("Digite sua String: ");
+    printf("Digite seu Texto: ");
     fgets(string, sizeof(string), stdin);
     
     for(i = 0; i < strlen(string); i++){
